3b33f1b552d570f6365fb3d767968336f600dc18
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-09-13 15:58:27 +0900

    Update hello_triangle.cpp

--- Getting Started/hello_triangle.cpp
+++ Getting Started/hello_triangle.cpp
@@ -34,6 +34,43 @@ int main()
         return -1;
     }    
 
+//	Define the triangle's coordinates in normalized device coordinates using a float array
+//	Each vertex has a z coordinate of 0 to make it look like it is 2D
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+/*	A VBO can store a large number of vertices in the GPUs memory. This buffer has a unique ID
+	corresponding to that buffer. We can generate one with a buffer ID using the glGenBuffers
+	function. */	
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+/*	The buffer type of a VBO is GL_ARRAY_BUFFER. OpenGL lets us bind to several buffers at once
+	provided they have a different buffer type. We can bind the newly created buffer to the
+	GL_ARRAY_BUFFER target with the glBindBuffer function: */
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+/*	Any buffer calls made on the array buffer will be used to configure the currently bound 
+	buffer, VBO. Then we can make a call to the glBufferData function that copies the previously
+	defined vertex data into the buffer's memory. */
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+/*	The first parameter is the type of buffer we want to copy data into. The second parameter
+	specifies the size of the data we want to pass to the buffer. The third parameter is the 
+	actual data to be sent. The last parameter specifies how we want the graphics card to manage
+	the given data:
+			GL_STREAM_DATA:
+				the data is set only once and used by the GPU at most a few times
+			GL_STATIC_DRAW:
+				the data is set only once and used many times
+			GL_DYNAMIC_DRAW:
+				the data is changed a lot and used many times
+	Now we will create the shaders. For this file the shaders will be hard coded as strings at 
+	the top of the file. */
+
+
+	
+	
     while (!glfwWindowShouldClose(window))
     {
 
