705a4ea23e623385122eb7254a70adc4ddbc89b0
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-10-13 16:50:30 +0900

    Update multiple_lights.cpp

--- Lighting/multiple_lights.cpp
+++ Lighting/multiple_lights.cpp
@@ -64,4 +64,78 @@ vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir)
 	return (ambient + diffuse * specular);
 }
 
+// Point Light
+
+struct PointLight {
+	vec3 position;
 	
+	float constant;
+	float linear;
+	float quadratic;
+	
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+#define NR_POINT_LIGHTS 4
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+/* 	We use a pre-procesor directive to define the number of point lights in the scene. Then we
+	use this constant to create an array of PointLight structs. The prototype is: */
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
+// Now the function itself:
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - fragPos);
+	// diffuse
+	float diff = max(dot(normal, lightDir), 0.0);
+	// specular
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	// attenuation
+	float distance = length(light.position, fragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + 
+							   light.quadratic * (distance * distance));
+	
+	// combine
+	vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+} 
+
+// Now we can put it all together in the main function!
+
+void main() {
+	// properties
+	vec3 norm = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - fragPos);
+	
+	// directional lighting
+	vec3 result = calcDirLighting(dirLight, norm, viewDir);
+	
+	// point lights
+	for (int i = 0; i < NR_POINT_LIGHTS; i++)
+		result += calcPointLight(pointLights[i], norm, fragPos, viewDir);
+	
+	fragColor = vec4(result, 1.0);
+}
+
+/*	Each light type adds its contribution to the resulting output color until all light sources
+	are processed. 
+	
+	How can we set the uniform values of the point lights since the point light uniform is 
+	an array of structs? It works like setting the uniforms of a single struct, only this time
+	we have to define the appropriate index when querying the uniform's location. */
+
+lightingShader.setFloat("pointLights[0].constant", 1.0f);
+			
+
+
